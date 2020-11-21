bb660bda8bfb36d05b74b36a7dd23f34fd9418ea
Jonathan Cameron <jmcameron@gmail.com>
2020-11-20 18:33:04 -0800

    Updated

--- keyboards/gothic70_jmc/keymaps/default/keymap.c
+++ keyboards/gothic70_jmc/keymaps/default/keymap.c
@@ -4,12 +4,12 @@
 // Define layers
 enum layers {
   NORMAL_LAYER = 0,
-  FN_LAYER     = 1,
-  MEDIA_LAYER  = 2
+  FN1_LAYER    = 1,
+  FN2_LAYER    = 2
 };
 #define FN_CAPS LT(1, KC_CAPS)
-#define FN1 MO(FN_LAYER)
-#define FN2 MO(MEDIA_LAYER)
+#define FN1 MO(FN1_LAYER)
+#define FN2 MO(FN2_LAYER)
 
 // Tap Dance Declarations
 enum {
@@ -18,18 +18,16 @@ enum {
 
 #define SCAPS_LG TD(TD_SCAPS)
 
-
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_GRV,   KC_BSPC,         KC_DEL,  \
+  [NORMAL_LAYER] = LAYOUT(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,    KC_BSPC,         KC_DEL,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,         KC_GRV,  \
     FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_CIRC, \
-    SCAPS_LG, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,     FN2,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,             KC_UP,                  \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     FN2,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,             KC_UP,                  \
     KC_LCTL, KC_LGUI,    KC_LALT,    KC_BSPC,    FN1,                   KC_SPC,           KC_RALT,   KC_RCTL,               KC_LEFT, KC_DOWN, KC_RGHT        \
   ),
 
-  [1] = LAYOUT(
+  [FN1_LAYER] = LAYOUT(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______,          KC_INS,  \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______,                   _______, \
     _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______,                            _______, \
@@ -37,11 +35,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______,    _______,    _______,    _______,              _______,          _______,          _______,         KC_HOME, KC_PGDN, KC_END         \
   ),
 
-  [2] = LAYOUT(
+  [FN2_LAYER] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_VOLU, \
     _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______,                   KC_VOLD, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                            KC_MSEL, \
-    _______, _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, _______,             KC_MSTP,                \
+    _______, _______, _______, KC_CALC, _______, _______, _______, KC_MUTE, _______, _______, _______, _______, _______,             KC_MSTP,                \
     _______, _______,    _______,     _______,   _______,              _______,          _______,          _______,         KC_MPRV, KC_MPLY, KC_MNXT        \
   )
 };

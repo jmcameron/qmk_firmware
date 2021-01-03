759418b4d6604fcbbbdd3f5abfb1f1467c255ff5
Jonathan Cameron <jmcameron@gmail.com>
2021-01-03 13:59:55 -0800

    Updated left space bar

--- keyboards/gothic70_jmc/keymaps/default/keymap.c
+++ keyboards/gothic70_jmc/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,  KC_BSLS,         KC_CIRC,  \
     FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,             KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                    KC_DEL, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     FN2,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,             KC_UP,                  \
-    KC_LCTL, KC_LGUI,    KC_LALT,    KC_BSPC,    FN1,                   KC_SPC,           KC_RALT,   KC_RCTL,               KC_LEFT, KC_DOWN, KC_RGHT        \
+    KC_LCTL, KC_LGUI,    KC_LALT,     KC_SPC,    FN1,                   KC_SPC,           KC_RALT,   KC_RCTL,               KC_LEFT, KC_DOWN, KC_RGHT        \
   ),
 
   [FN1_LAYER] = LAYOUT(
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______,                   _______, \
     _______, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______, _______, _______, _______,                            KC_INS, \
     _______, RGB_TOG, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______, KC_MUTE, _______, _______, _______, _______, _______,             KC_PGUP,                \
-    _______, _______,    _______,    KC_DEL,     _______,              _______,          _______,          _______,         KC_HOME, KC_PGDN, KC_END         \
+    _______, _______,    _______,    _______,    _______,              _______,          _______,          _______,         KC_HOME, KC_PGDN, KC_END         \
   ),
 
   [FN2_LAYER] = LAYOUT(
